718007afff21fccd304f4b2df4b57cb83ff58582
nerggg <13522147@std.stei.itb.ac.id>
2024-05-01 21:19:55 +0700

    refactor: database change

--- Main.java
+++ Main.java
@@ -7,35 +7,18 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BFS bfs = new BFS();
         Map<String, Boolean> db = new HashMap<>();
-        Scanner sc = new Scanner(new File("english.csv"));
-        sc.useDelimiter(",");
+        Scanner sc = new Scanner(new File("words_alpha.txt"));
         while (sc.hasNext()) {
             String temp = sc.next();
-            int delIdx;
-
-            if (temp.length() == 0) {
-                continue;
-            } else {
-                temp = temp.substring(2);
-            }
-
-            while (temp.indexOf(' ') != -1) {
-                delIdx = temp.indexOf(' ');
-                temp = temp.substring(0, delIdx);
-            }
-            while (temp.indexOf('\n') != -1) {
-                delIdx = temp.indexOf('\n');
-                temp = temp.substring(0, delIdx);
-            }
-
-            if (temp.length() > 0 && !db.containsKey(temp)) {
+            if (!db.containsKey(temp)) {
                 db.put(temp.toLowerCase(), true);
             }
         }
         sc.close();
 
-        // int tes = bfs.ladderLength("vast", "vest", db);
+        int tes = bfs.ladderLength("vast", "vest", db);
         int tes = bfs.ladderLength("east", "west", db);
+        int tes = bfs.ladderLength("rime", "rice", db);
         System.out.println(tes);
     }
 }
