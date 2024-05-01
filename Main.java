0a68fd993fe5bfebfdbcf808debefec72164cedc
nerggg <13522147@std.stei.itb.ac.id>
2024-05-01 23:43:20 +0700

    add: resetDB function

--- Main.java
+++ Main.java
@@ -3,6 +3,17 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Main {
+    public static void resetDB(Map<String, Boolean> db, Scanner sc) throws Exception {
+        sc = new Scanner(new File("words_alpha.txt"));
+        while (sc.hasNext()) {
+            String temp = sc.next();
+            if (!db.containsKey(temp)) {
+                db.put(temp.toLowerCase(), true);
+            }
+        }
+        sc.close();
+    }
+
     public static void main(String[] args) throws Exception {
         BFS bfs = new BFS();
         GBFS gbfs = new GBFS();
@@ -19,24 +30,49 @@ public class Main {
         sc.close();
 
         long startTime = System.currentTimeMillis();
-
-        // List<String> tes = bfs.algo("charge", "comedo", db);
-        // System.out.println("hasil\n");
-
-        List<String> tes = gbfs.algo("charge", "comedo", db);
-        System.out.println("hasil\n");
-
-        // List<String> tes = ucs.algo("charge", "comedo", db);
-        // System.out.println("hasil\n");
-
-        // List<String> tes = AStar.algo("charge", "comedo", db);
-        // System.out.println("hasil\n");
-
+        List<String> tes = bfs.algo("charge", "comedo", db);
+        System.out.println("hasil BFS");
+        tes.forEach(item -> System.out.println(item));
         long endTime = System.currentTimeMillis();
         long totalTime = endTime - startTime;
-
-        tes.forEach(item -> System.out.println(item));
         System.out.println("panjang path " + tes.size());
         System.out.println("Total waktu yang dibutuhkan: " + totalTime + " milidetik");
+        System.out.println();
+
+        resetDB(db, sc);
+
+        startTime = System.currentTimeMillis();
+        List<String> tes2 = gbfs.algo("charge", "comedo", db);
+        System.out.println("hasil GBFS");
+        tes2.forEach(item -> System.out.println(item));
+        endTime = System.currentTimeMillis();
+        totalTime = endTime - startTime;
+        System.out.println("panjang path " + tes2.size());
+        System.out.println("Total waktu yang dibutuhkan: " + totalTime + " milidetik");
+        System.out.println();
+
+        resetDB(db, sc);
+
+        startTime = System.currentTimeMillis();
+        List<String> tes3 = ucs.algo("charge", "comedo", db);
+        System.out.println("hasil UCS");
+        tes3.forEach(item -> System.out.println(item));
+        endTime = System.currentTimeMillis();
+        totalTime = endTime - startTime;
+        System.out.println("panjang path " + tes3.size());
+        System.out.println("Total waktu yang dibutuhkan: " + totalTime + " milidetik");
+        System.out.println();
+
+        resetDB(db, sc);
+
+        startTime = System.currentTimeMillis();
+        List<String> tes4 = AStar.algo("charge", "comedo", db);
+        System.out.println("hasil A*");
+        tes4.forEach(item -> System.out.println(item));
+        endTime = System.currentTimeMillis();
+        totalTime = endTime - startTime;
+        System.out.println("panjang path " + tes4.size());
+        System.out.println("Total waktu yang dibutuhkan: " + totalTime + " milidetik");
+        System.out.println();
     }
 }
