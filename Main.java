066f94ac2edb846e9dc220e609ff5d8b3b56290a
nerggg <13522147@std.stei.itb.ac.id>
2024-05-02 10:56:23 +0700

    chore: input dari cli

--- Main.java
+++ Main.java
@@ -29,20 +29,42 @@ public class Main {
         }
         sc.close();
 
+        Scanner userinput = new Scanner(System.in);
+        boolean flag = false;
+        String beginWord = "";
+        String endWord = "";
+
+        while (!flag) {
+            System.out.println("Masukkan kata awal");
+            beginWord = userinput.next();
+            System.out.println("Masukkan kata akhir");
+            endWord = userinput.next();
+            beginWord = beginWord.toLowerCase();
+            endWord = endWord.toLowerCase();
+            if (beginWord.length() != endWord.length()) {
+                System.out.println("Panjang kedua kata harus sama!");
+            } else if (!db.containsKey(beginWord) || !db.containsKey(endWord)) {
+                System.out.println("Kata yang dimasukkan harus kata dalam bahasa inggris!");
+            } else {
+                flag = true;
+            }
+        }
+        userinput.close();
+
         long startTime = System.currentTimeMillis();
-        List<String> tes = bfs.algo("charge", "comedo", db);
+        List<String> tes1 = bfs.algo(beginWord, endWord, db);
         System.out.println("hasil BFS");
-        tes.forEach(item -> System.out.println(item));
+        tes1.forEach(item -> System.out.println(item));
         long endTime = System.currentTimeMillis();
         long totalTime = endTime - startTime;
-        System.out.println("panjang path " + tes.size());
+        System.out.println("panjang path " + tes1.size());
         System.out.println("Total waktu yang dibutuhkan: " + totalTime + " milidetik");
         System.out.println();
 
         resetDB(db, sc);
 
         startTime = System.currentTimeMillis();
-        List<String> tes2 = gbfs.algo("charge", "comedo", db);
+        List<String> tes2 = gbfs.algo(beginWord, endWord, db);
         System.out.println("hasil GBFS");
         tes2.forEach(item -> System.out.println(item));
         endTime = System.currentTimeMillis();
@@ -54,7 +76,7 @@ public class Main {
         resetDB(db, sc);
 
         startTime = System.currentTimeMillis();
-        List<String> tes3 = ucs.algo("charge", "comedo", db);
+        List<String> tes3 = ucs.algo(beginWord, endWord, db);
         System.out.println("hasil UCS");
         tes3.forEach(item -> System.out.println(item));
         endTime = System.currentTimeMillis();
@@ -66,7 +88,7 @@ public class Main {
         resetDB(db, sc);
 
         startTime = System.currentTimeMillis();
-        List<String> tes4 = AStar.algo("charge", "comedo", db);
+        List<String> tes4 = AStar.algo(beginWord, endWord, db);
         System.out.println("hasil A*");
         tes4.forEach(item -> System.out.println(item));
         endTime = System.currentTimeMillis();
