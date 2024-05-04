7d93a090e797dcb97d12f188a84e977265d94bb8
nerggg <13522147@std.stei.itb.ac.id>
2024-05-04 23:31:39 +0700

    refactor: code cleaning

--- src/UCS.java
+++ src/UCS.java
@@ -7,7 +7,6 @@ class UCS {
 
         PriorityQueue<NodeUCS> todo = new PriorityQueue<>((a, b) -> Integer.compare(a.cost, b.cost));
         todo.add(beginNode);
-
         while (!todo.isEmpty()) {
             NodeUCS current = todo.poll();
             if (current.word.equals(endWord)) {
@@ -24,7 +23,7 @@ class UCS {
                     if (wordList.containsKey(newWord)) {
                         List<String> tempPath = new ArrayList<>(current.path);
                         tempPath.add(newWord);
-                        int newCost = current.cost + 1; // Biaya untuk UCS adalah 1
+                        int newCost = current.cost + 1;
                         NodeUCS newNode = new NodeUCS(newWord, newCost, tempPath);
                         todo.add(newNode);
                         wordList.remove(newWord);
